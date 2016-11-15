b37f7b833b175008f37175b1a7776d3ab8cc8eb0
Tho Cao <Tho Cao@DESKTOP-9ICJCD4>
2016-11-16 00:36:42 +0900

    Add project files.

---
+++ Hello World/Main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+int main() {
+
+	while (1){
+
+		std::cout << "Hello world" << std::endl;
+		std::cout << "My name's Tho Cao " << std::endl;
+		break;
+	}
+	while (true)
+	{
+
+	}
+	return 0;
+}
