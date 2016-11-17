9c7420092da73b10b388447a5d36ed893295d2a7
Tho Cao <thocao92@gmail.com>
2016-11-18 02:07:30 +0900

    Add fucntion for calculate vector

--- Hello World/Main.cpp
+++ Hello World/Main.cpp
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <iostream>
+#include <assert.h>
+// this header for SSE compiler
+#include <immintrin.h>
+
 int main() {
+
+	int *buffer;
+	int *buffer_maloc;
+	buffer_maloc = (int*)_aligned_malloc(sizeof(int) * 1000, 16);
+	// first buufer
+	buffer = new  int[1000];
+	memset(buffer, 100, 1000);
+	__m128i zero = _mm_setzero_si128();
+	for (__m128i *i = reinterpret_cast<__m128i*>(buffer), *end = reinterpret_cast<__m128i*>(&buffer[1000]);i < end;i++) {
+		_mm_store_si128(i, zero);
+	}
+	// second buffer
+	const __m128i value_maloc = _mm_set1_epi32(10);
+	for (__m128i *i = reinterpret_cast<__m128i*>(buffer_maloc), *end = reinterpret_cast<__m128i*>(&buffer_maloc[1000]);i < end;i++) {
+		_mm_store_si128(i, value_maloc);
+	}
+
+	for (unsigned int id = 0;id != 1000;id++) {
+		std::cout << " " << buffer_maloc[id] << std::endl;
+	}
+
+	delete[] buffer;
+	/*delete buffer_maloc;*/
 
 	while (1){
 
@@ -10,7 +37,9 @@ int main() {
 	}
 	while (true)
 	{
-
+		//std::cout << "Now I want to see the different things when i use Github.com" << std::endl;
 	}
+
+
 	return 0;
 }
