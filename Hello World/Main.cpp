a593200c609b9cc14962151630f9626c9c578660
Tho Cao <thocao92@gmail.com>
2016-11-19 00:24:57 +0900

    AVX 256 Instructor

--- Hello World/Main.cpp
+++ Hello World/Main.cpp
@@ -29,6 +29,16 @@ void SumVector(int n, float alpha, float *X, float *Y) {
 	}
 }
 
+void SumVectorAVX(int n, float alpha, float *X, float *Y) {
+	__m256 x_vec, y_vec, res_vec;
+	const __m256 a_vec = _mm256_set1_ps(alpha);
+	for (int i = 0; i < n; i += 8) {
+		x_vec = _mm256_load_ps(&X[i]);
+		y_vec = _mm256_load_ps(&Y[i]);
+		res_vec = _mm256_add_ps(_mm256_mul_ps(a_vec, x_vec), y_vec);
+		_mm256_store_ps(&Y[i], res_vec);
+	}
+}
 int main() {
 
 	int *buffer;
